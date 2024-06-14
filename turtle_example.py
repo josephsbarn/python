84ff504b91d1275e45fe2f77c66b656ca23ebab7
seunghan Ryoo <148834180+josephsbarn@users.noreply.github.com>
2024-06-13 23:06:49 -0700

    Create turtle_example.py

---
+++ turtle_example.py
@@ -0,0 +1,40 @@
+# importing packages 
+import turtle 
+  
+# Create a screen
+screen = turtle.Screen()
+screen.title("Turtle Junle")
+screen.bgcolor("white")
+
+# Create a turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("green")
+
+# Move the turtle ("+" shape)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+# Keep the window open
+turtle.done()
